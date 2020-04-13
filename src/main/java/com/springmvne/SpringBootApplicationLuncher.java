63877294cdf688c57eaeade438b65e6b05c6da87
anoopbajpainiit <anoop.bajpai@niit-tech.com>
2020-04-13 15:54:31 +0530

    Project source commit

---
+++ src/main/java/com/springmvne/SpringBootApplicationLuncher.java
@@ -0,0 +1,13 @@
+package com.springmvne;
+import org.springframework.boot.SpringApplication;
+/**
+ * This Luncher for the spring boot application.
+ *
+ * @author manoj.bardhan
+ *
+ */
+public class SpringBootApplicationLuncher {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldController.class, args);
+    }
+}
